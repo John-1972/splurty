b640f1f776650940e6c79df49e969e1fb13772a4
John Henderson <johnhenderson@fastmail.fm>
2020-01-10 17:50:26 +0000

    Getting this app back up & running after many months

--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180214213731) do
+ActiveRecord::Schema.define(version: 2018_02_14_213731) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "quotes", force: :cascade do |t|
-    t.string   "saying"
-    t.string   "author"
+  create_table "quotes", id: :serial, force: :cascade do |t|
+    t.string "saying"
+    t.string "author"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Quote.create(saying: 'I knew I was going bald when it was taking longer and longer to wash my face.', author: 'Harry Hill')
+Quote.create(saying: 'My neighbour asked if he could use my lawnmower and I told him of course he could, so long as he didn\'t take it out of my garden.', author: 'Eric Morecambe')
+Quote.create(saying: 'I told the Inland Revenue that I didn\'t owe them a penny because I lived near the seaside.', author: 'Ken Dodd')
+Quote.create(saying: 'Last year, my wife ran off with the fellow next door, and I must admit... I still miss him.', author: 'Les Dawson')
+Quote.create(saying: 'My father only hit me once. But he used a Volvo.', author: 'Bob Monkhouse')
+Quote.create(saying: 'I wasn\'t a very athletic boy. I was once lapped in the long jump.', author: 'Ronnie Corbett')
+Quote.create(saying: 'I\'ve been on the whiskey diet. I\'ve already lost three days.', author: 'Tommy Cooper')
+Quote.create(saying: 'On quiet nights, when I\'m alone, I like to run our wedding video backwards, just to watch myself walk out of the church a free man.', author: 'Jim Davidson')
+Quote.create(saying: 'Those hot pants of hers were so damned tight, I could hardly breathe.', author: 'Benny Hill')
+Quote.create(saying: 'About a month before he died, we rubbed lard on my grandfather\'s back. After that, he went downhill very quickly.', author: 'Milton Jones')
